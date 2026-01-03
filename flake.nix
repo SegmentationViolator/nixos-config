6d8b2b67392e5efaa23a15e80fac75a895ea9109
SegmentationViolator <segmentationviolator@proton.me>
2026-01-03 20:19:29 +0530

    fixed improper merge

--- flake.nix
+++ flake.nix
@@ -1,36 +1,4 @@
 {
-<<<<<<< HEAD
-  description = "NixOS configuration";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nvf = {
-      url = "github:NotAShelf/nvf/v0.8";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  outputs = { nixpkgs, home-manager, nvf, ... }:
-  {
-    nixosConfigurations.segmentedbox = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.ale = users/ale/home.nix;
-        }
-        nvf.nixosModules.default
-      ];
-    };
-  };
-=======
     description = "NixOS configuration";
 
     inputs = {
@@ -98,5 +66,4 @@
             };
         };
     };
->>>>>>> nixos-config/master
 }
