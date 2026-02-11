3ac8020a8051de654be609af2bb3afbd477896ac
Segmentation Violator <segmentationviolator@proton.me>
2026-02-12 00:38:24 +0530

    chore: bump flake inputs

--- flake.nix
+++ flake.nix
@@ -3,23 +3,12 @@
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-        nvf-config = {
-            url = "github:SegmentationViolator/nvf-config";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
     };
 
-    outputs = { nixpkgs, nvf-config, ... }: {
+    outputs = { nixpkgs, ... }: {
         nixosConfigurations = {
             segmented-box = nixpkgs.lib.nixosSystem {
                 modules = [ ./hosts/segmented-box ];
-            };
-
-            segmented-usb = nixpkgs.lib.nixosSystem {
-                modules = [ ./hosts/segmented-usb ];
-                specialArgs = { inherit nvf-config; };
-                system = "x86_64-linux";
             };
         };
     };
